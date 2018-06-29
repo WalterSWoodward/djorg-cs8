c6968a5bd5b939f66f6903e9206a3bf70e1d21c6
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-06-29 13:19:34 -0400

    Debugging #8
    Trying to debug this Error: django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet.

--- djorg/settings.py
+++ djorg/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import django
+django.setup()
 import os
 import dj_database_url
 from decouple import config # Gives us access to config library
