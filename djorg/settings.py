ee16215ebe2788b3d65143bfbf3414596feb80d1
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-06-29 17:31:14 -0400

    Write ALLOWED_HOSTS properly

--- djorg/settings.py
+++ djorg/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool) # Need this to convert incoming type string to bool
 
-ALLOWED_HOSTS = ['*']
-
+# ALLOWED_HOSTS = ['*'] # Not secure
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
